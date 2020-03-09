1369cdb9323c98ef59a0a0f83296e20ced9b8500
Christopher Stone <chstone@microsoft.com>
2020-03-09 11:23:47 -0400

    updates

--- Program.cs
+++ Program.cs
@@ -26,10 +26,36 @@ namespace forms_sdk_test
             var receipts = new ReceiptClient(frEndpoint, frKey, frOptions);
             var layout = new FormLayoutClient(frEndpoint, frKey, frOptions);
 
-            //   await TrainCustomModelWithLabels(custom);
-            //   await AnalyzeWithCustomModelWithLabels(custom);
+            // await TrainCustomModelWithLabels(custom);
+            // await AnalyzeWithCustomModelWithLabels(custom);
+            // await AnalyzeLayout(layout);
+            // await AnalyzeReceipt(receipts);
+            // await GetModelSummary(custom);
+            // await DeleteModel(custom);
 
             await Task.CompletedTask;
+        }
+
+        // static async Task GetModel(CustomFormClient client)
+        // {
+        //     var modelId = "91c8da74-3bde-44e5-9fd4-3d8ae150c0bf";
+        //     var resp = client.StartTrainingAsync()
+        // }
+
+        static async Task DeleteModel(CustomFormClient client)
+        {
+            var modelId = "51ee990c-bb72-49c5-a31f-3a7f62bfc40b";
+            var resp = await client.DeleteModelAsync(modelId);
+            Console.WriteLine("Deleted.");
+        }
+
+        static async Task GetModelSummary(CustomFormClient client)
+        {
+            var resp = await client.GetModelSubscriptionPropertiesAsync();
+            var result = resp.Value;
+            Console.WriteLine($"Count: {result.Count}");
+            Console.WriteLine($"Limit: {result.Limit}");
+            Console.WriteLine($"LastUpdated: {result.LastUpdatedOn}");
         }
 
         static async Task GetCustomModelPage(CustomFormClient client)
@@ -84,6 +110,86 @@ namespace forms_sdk_test
                     Console.WriteLine($"{error.Code} | {error.Message}");
                 }
             }
+        }
+
+        static async Task AnalyzeLayout(FormLayoutClient client)
+        {
+            var filePath = "/Users/chstone/Downloads/sample_data/Test/Invoice_6.pdf";
+            using var stream = File.OpenRead(filePath);
+            var poller = await client.StartExtractLayoutAsync(stream, FormContentType.Pdf, true);
+            var resp = await poller.WaitForCompletionAsync(TimeSpan.FromSeconds(1));
+            var result = resp.Value;
+            foreach (var page in result)
+            {
+                Console.WriteLine($"- Page#: {page.PageNumber}");
+                Console.WriteLine($"  Angle: {page.RawExtractedPage.Angle}");
+                Console.WriteLine($"  Width: {page.RawExtractedPage.Width} {page.RawExtractedPage.Unit}");
+                Console.WriteLine($"  Height: {page.RawExtractedPage.Height} {page.RawExtractedPage.Unit}");
+                Console.WriteLine($"  Lang: {page.RawExtractedPage.Language}");
+                Console.WriteLine($"  Page#: {page.RawExtractedPage.Page}");
+                Console.WriteLine($"  LineCount: {page.RawExtractedPage.Lines.Count}");
+                Console.WriteLine($"  Tables:");
+                foreach (var table in page.Tables)
+                {
+                    Console.WriteLine($"  - Dims: {table.ColumnCount} x {table.RowCount}");
+                    Console.WriteLine($"    Cells:");
+                    foreach (var cell in table.Cells)
+                    {
+                        Console.WriteLine($"    - Text: {cell.Text}");
+                        Console.WriteLine($"      Col: {cell.ColumnIndex}");
+                        Console.WriteLine($"      Row: {cell.RowIndex}");
+                        Console.WriteLine($"      ColSpan: {cell.ColumnSpan}");
+                        Console.WriteLine($"      RowSpan: {cell.RowSpan}");
+                        Console.WriteLine($"      Confidence: {cell.Confidence}");
+                        Console.WriteLine($"      Head: {cell.IsHeader}");
+                        Console.WriteLine($"      Foot: {cell.IsFooter}");
+                        Console.WriteLine($"      Box: {GetBoundingBoxString(cell.BoundingBox)}");
+                    }
+                }
+            }
+        }
+
+        static async Task AnalyzeReceipt(ReceiptClient client)
+        {
+            var filePath = "/Users/chstone/Downloads/contoso-allinone.jpg";
+            using var stream = File.OpenRead(filePath);
+            var resp = await client.ExtractReceiptAsync(stream, FormContentType.Jpeg, true);
+            var result = resp.Value;
+            Console.WriteLine($"Receipt:");
+            Console.WriteLine($"  Start: {result.PageRange.StartPageNumber}");
+            Console.WriteLine($"  End: {result.PageRange.EndPageNumber}");
+            Console.WriteLine($"  Type: {result.ReceiptType}");
+            Console.WriteLine($"  MerchantName: {result.MerchantName}");
+            Console.WriteLine($"  MerchantAddress: {result.MerchantAddress}");
+            Console.WriteLine($"  MerchantPhoneNumber: {result.MerchantPhoneNumber}");
+            Console.WriteLine($"  TransactionDate: {result.TransactionDate}");
+            Console.WriteLine($"  TransactionTime: {result.TransactionTime}");
+            Console.WriteLine($"  TransactionDate2: {result.ExtractedFields["TransactionDate"].Text}");
+            Console.WriteLine($"  Subtotal: {result.Subtotal}");
+            Console.WriteLine($"  Tax: {result.Tax}");
+            Console.WriteLine($"  Tip: {result.Tip}");
+            Console.WriteLine($"  Total: {result.Total}");
+            Console.WriteLine($"  Items:");
+            foreach (var item in result.Items)
+            {
+                Console.WriteLine($"  - Name: {item.Name}");
+                Console.WriteLine($"  - Quantity: {item.Quantity}");
+                // Console.WriteLine($"  - Price: {item.Price}");
+                Console.WriteLine($"  - TotalPrice: {item.TotalPrice}");
+            }
+            Console.WriteLine($"  Page:");
+            Console.WriteLine($"    Number: {result.RawExtractedPage.Page}");
+            Console.WriteLine($"    Width: {result.RawExtractedPage.Width} {result.RawExtractedPage.Unit}");
+            Console.WriteLine($"    Height: {result.RawExtractedPage.Height} {result.RawExtractedPage.Unit}");
+            Console.WriteLine($"    Angle: {result.RawExtractedPage.Angle}");
+            Console.WriteLine($"    Language: {result.RawExtractedPage.Language}");
+            Console.WriteLine($"    LineCount: {result.RawExtractedPage.Lines.Count}");
+        }
+
+        static string GetBoundingBoxString(BoundingBox box)
+        {
+            return string.Join(", ", box.Points
+                .Select((p) => $"({p.X}, {p.Y})"));
         }
 
         static async Task AnalyzeWithCustomModel(CustomFormClient client)
